fe546ac054c308fd5416e945465f0d6243750746
eddie-six <46092045+eddie-six@users.noreply.github.com>
2018-12-26 19:57:13 -0500

    Adding timer component

--- playground/src/App.js
+++ playground/src/App.js
@@ -26,3 +26,37 @@ class App extends Component {
 }
 
 export default App;
+
+class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { seconds: 0, minutes:0, allSeconds:0};
+  }
+
+  tick() {
+    this.setState(state => ({
+      seconds: state.allSeconds == 60 ? 0 : state.seconds + 1,
+      minutes: parseInt(new Number(state.allSeconds/60)),
+      allSeconds: state.allSeconds + 1
+
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div>
+        Seconds: {this.state.seconds}, Minutes: {this.state.minutes}, Total Seconds: {this.state.allSeconds}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Timer />, mountNode);
